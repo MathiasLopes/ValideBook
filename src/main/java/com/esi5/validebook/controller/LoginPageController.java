914b7b300437cc2fce8c44c708ea1290673dc163
Mathias Lopes Fernandes <mathiaslopesfernandes@outlook.fr>
2021-01-07 17:16:59 +0100

    add index page

--- src/main/java/com/esi5/validebook/controller/LoginPageController.java
+++ src/main/java/com/esi5/validebook/controller/LoginPageController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginPageController {
 
+    @GetMapping("/")
+    public String index(){
+        return "account/login";
+    }
+
     @GetMapping("/login")
     public String login(){
         return "account/login";
