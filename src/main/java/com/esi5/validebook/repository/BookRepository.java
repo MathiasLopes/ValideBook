2cb4a96e9e7baa12a8df8f0969654371ac20473a
Mathias Lopes Fernandes <mathiaslopesfernandes@outlook.fr>
2021-01-30 19:18:41 +0100

    [FIX] Correction utilisation des requetes

--- src/main/java/com/esi5/validebook/repository/BookRepository.java
+++ src/main/java/com/esi5/validebook/repository/BookRepository.java
@@ -14,28 +14,31 @@ import com.esi5.validebook.entity.BookEntity;
 @Repository
 public interface BookRepository extends JpaRepository<BookEntity, Long> {
 
-    @Query(value="select * from book where book.datevalidation IS NOT NULL ORDER BY book.id DESC LIMIT :limit", nativeQuery=true)
+    String selectAllBook = "select * from book";
+    String bookOrderByDesc = " ORDER BY book.id DESC";
+
+    @Query(value= selectAllBook + " where book.datevalidation IS NOT NULL " + bookOrderByDesc + " LIMIT :limit", nativeQuery=true)
     List<BookEntity> getXLastBookValidate(@Param("limit") Integer limit);
 
-    @Query(value="select * from book where book.datevalidation IS NOT NULL and titre like :titre", nativeQuery=true)
+    @Query(value= selectAllBook + " book where book.datevalidation IS NOT NULL and titre like :titre " + bookOrderByDesc, nativeQuery=true)
     List<BookEntity> getWithTitre(@Param("titre") String titre);
 
-    @Query(value="select * from book, langues where book.datevalidation IS NOT NULL and book.idlangue = langues.id and langues.langue like :langue", nativeQuery=true)
+    @Query(value= selectAllBook + ", langues where book.datevalidation IS NOT NULL and book.idlangue = langues.id and langues.langue like :langue " + bookOrderByDesc, nativeQuery=true)
     List<BookEntity> getWithLangue(@Param("langue") String langue);
 
-    @Query(value="select * from book, langues where book.datevalidation IS NOT NULL and book.idlangue = langues.id and book.titre like :titre and langues.langue like :langue", nativeQuery=true)
+    @Query(value= selectAllBook + ", langues where book.datevalidation IS NOT NULL and book.idlangue = langues.id and book.titre like :titre and langues.langue like :langue " + bookOrderByDesc, nativeQuery=true)
     List<BookEntity> getWithTitreAndLangue(@Param("titre") String titre, @Param ("langue") String langue);
 
-    @Query(value="select * from book where book.datevalidation IS NOT NULL and datepublication >= :datepublicationdebut and datepublication < :datepublicationfin", nativeQuery=true)
+    @Query(value= selectAllBook + " where book.datevalidation IS NOT NULL and datepublication >= :datepublicationdebut and datepublication < :datepublicationfin " + bookOrderByDesc, nativeQuery=true)
     List<BookEntity> getWithDatePublication(@Param("datepublicationdebut") String datepublicationdebut, @Param("datepublicationfin") String datepublicationfin);
 
-    @Query(value="select * from book where book.datevalidation IS NOT NULL and titre like :titre and datepublication >= :datepublicationdebut and datepublication < :datepublicationfin", nativeQuery=true)
+    @Query(value= selectAllBook + " where book.datevalidation IS NOT NULL and titre like :titre and datepublication >= :datepublicationdebut and datepublication < :datepublicationfin " + bookOrderByDesc, nativeQuery=true)
     List<BookEntity> getWithTitreAndDatePublication(@Param("titre") String titre, @Param("datepublicationdebut") String datepublicationdebut, @Param("datepublicationfin") String datepublicationfin);
 
-    @Query(value="select * from book, langues where book.datevalidation IS NOT NULL and book.idlangue = langues.id and langues.langue like :langue and book.datepublication >= :datepublicationdebut and book.datepublication < :datepublicationfin", nativeQuery=true)
+    @Query(value= selectAllBook + ", langues where book.datevalidation IS NOT NULL and book.idlangue = langues.id and langues.langue like :langue and book.datepublication >= :datepublicationdebut and book.datepublication < :datepublicationfin " + bookOrderByDesc, nativeQuery=true)
     List<BookEntity> getWithLangueAndDatePublication(@Param("langue") String langue, @Param("datepublicationdebut") String datepublicationdebut, @Param("datepublicationfin") String datepublicationfin);
 
-    @Query(value="select * from book, langues where book.datevalidation IS NOT NULL and book.idlangue = langues.id and langues.langue like :langue and book.titre like :titre and book.datepublication >= :datepublicationdebut and book.datepublication < :datepublicationfin", nativeQuery=true)
+    @Query(value= selectAllBook + ", langues where book.datevalidation IS NOT NULL and book.idlangue = langues.id and langues.langue like :langue and book.titre like :titre and book.datepublication >= :datepublicationdebut and book.datepublication < :datepublicationfin " + bookOrderByDesc, nativeQuery=true)
     List<BookEntity> getWithTitreAndLangueAndDatePublication(@Param("titre") String titre, @Param("langue") String langue, @Param("datepublicationdebut") String datepublicationdebut, @Param("datepublicationfin") String datepublicationfin);
 
 }
