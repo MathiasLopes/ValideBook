f6b840e82171978a3f7588ba0520a663ce168aa7
Mathias Lopes Fernandes <mathiaslopesfernandes@outlook.fr>
2021-03-11 12:21:16 +0100

    [FEAT] Ajout UnitTest GetUserByUsername

--- src/test/java/com/esi5/validebook/ValidebookApplicationTests.java
+++ src/test/java/com/esi5/validebook/ValidebookApplicationTests.java
@@ -1,13 +1,22 @@
 package com.esi5.validebook;
 
+import com.esi5.validebook.service.CustomUserDetailService;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @SpringBootTest
 class ValidebookApplicationTests {
 
 	@Test
 	void contextLoads() {
+
+	}
+
+	@Test
+	void GetUserByUsername(){
+		new CustomUserDetailService().loadUserByUsername("mathias@test.fr");
 	}
 
 }
