ac734ab7d02304b6f154767402998f111ffa5423
Mathias Lopes Fernandes <mathiaslopesfernandes@outlook.fr>
2021-04-15 14:15:48 +0200

    [FEAT] test u "GetEditeurList" replaced by "GetThemeList"

--- src/test/java/com/esi5/validebook/ValidebookApplicationTests.java
+++ src/test/java/com/esi5/validebook/ValidebookApplicationTests.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.esi5.validebook.entity.BookEntity;
 import com.esi5.validebook.entity.EditeurEntity;
+import com.esi5.validebook.entity.ThemeEntity;
 import com.esi5.validebook.entity.ForWebRequest.BookEntityRequest;
 import com.esi5.validebook.repository.*;
 
@@ -20,7 +21,7 @@ class ValidebookApplicationTests {
 	@Autowired
 	BookRepository bookRepository;
 	@Autowired
-	EditeurRepository editeurRepository;
+	ThemeRepository themeRepository;
 
 	@Test
 	void contextLoads() {
@@ -41,12 +42,11 @@ class ValidebookApplicationTests {
 			throw new Exception("Erreur : Le livre avec l'id 1 n'a pas pu être récupéré");
 	}
 
-	//for commit push jenkins
 	@Test
-	void GetListEditeurs() throws Exception{
-		List<EditeurEntity> editeurList = editeurRepository.findAll();
-		if(editeurList != null || editeurList.size() <= 0)
-			throw new Exception("Erreur : Aucun editeur retrouvé");
+	void GetListThemes() throws Exception{
+		List<ThemeEntity> themeList = themeRepository.findAll();
+		if(themeList == null || themeList.size() <= 0)
+			throw new Exception("Erreur : Aucun theme retrouvé");
 	}
 
 }
