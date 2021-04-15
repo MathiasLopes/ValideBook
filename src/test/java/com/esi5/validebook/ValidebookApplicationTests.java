e3beeb2d3039f7c58ffb734de6019519b30346ec
Mathias Lopes Fernandes <mathiaslopesfernandes@outlook.fr>
2021-04-15 12:45:18 +0200

    [FEAT] remplacement du test U avec recupere de tous les livres

--- src/test/java/com/esi5/validebook/ValidebookApplicationTests.java
+++ src/test/java/com/esi5/validebook/ValidebookApplicationTests.java
@@ -1,13 +1,22 @@
 package com.esi5.validebook;
 
-import com.esi5.validebook.service.CustomUserDetailService;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.esi5.validebook.entity.BookEntity;
+import com.esi5.validebook.repository.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 
-@SpringBootTest
+@SpringBootTest()
 class ValidebookApplicationTests {
+
+	@Autowired
+	BookRepository bookRepository;
 
 	@Test
 	void contextLoads() {
@@ -15,8 +24,10 @@ class ValidebookApplicationTests {
 	}
 
 	@Test
-	void GetUserByUsername(){
-		new CustomUserDetailService().loadUserByUsername("mathias@test.fr");
+	void GetBookList() throws Exception{
+		List<BookEntity> bookList = bookRepository.findAll();
+		if(bookList == null || bookList.size() > 0)
+			throw new Exception("Erreur : Aucun livre retrouvé");
 	}
 
 }
