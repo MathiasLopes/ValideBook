1f69af065683b4ac17034c3a5599cc57b82474d3
Mathias Lopes Fernandes <mathiaslopesfernandes@outlook.fr>
2021-06-04 10:45:15 +0200

    [UPDATE] mise à jour des test unit pour l'utilisation des methodes de l'application

--- src/test/java/com/esi5/validebook/ValidebookApplicationTests.java
+++ src/test/java/com/esi5/validebook/ValidebookApplicationTests.java
@@ -3,6 +3,7 @@ package com.esi5.validebook;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.esi5.validebook.controller.HomeRequestController;
 import com.esi5.validebook.entity.BookEntity;
 import com.esi5.validebook.entity.EditeurEntity;
 import com.esi5.validebook.entity.ThemeEntity;
@@ -22,17 +23,14 @@ class ValidebookApplicationTests {
 	BookRepository bookRepository;
 	@Autowired
 	ThemeRepository themeRepository;
+	@Autowired
+	EditeurRepository editeurRepository;
+	@Autowired
+	HomeRequestController homeRequestController;
 
 	@Test
 	void contextLoads() {
 
-	}
-
-	@Test
-	void GetBookList() throws Exception{
-		List<BookEntity> bookList = bookRepository.findAll();
-		if(bookList == null || bookList.size() <= 0)
-			throw new Exception("Erreur : Aucun livre retrouvé");
 	}
 
 	@Test
@@ -44,9 +42,16 @@ class ValidebookApplicationTests {
 
 	@Test
 	void GetListThemes() throws Exception{
-		List<ThemeEntity> themeList = themeRepository.findAll();
+		List<ThemeEntity> themeList = homeRequestController.listethemes();
 		if(themeList == null || themeList.size() <= 0)
 			throw new Exception("Erreur : Aucun theme retrouvé");
+	}
+
+	@Test
+	void GetBookList() throws Exception{
+		List<BookEntityRequest> listBook = homeRequestController.listebook("francais");
+		if(listBook == null || listBook.size() <= 0)
+			throw new Exception("Erreur : Aucun livre retrouvé");
 	}
 
 }
